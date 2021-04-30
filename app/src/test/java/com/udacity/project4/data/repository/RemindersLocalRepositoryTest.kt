b0d956c3f0c68d7542703df2fd7df50db2e7977e
Micheal Murray <michealmurray16@gmail.com>
2021-04-29 19:54:32 -0500

    Working on repository testing

--- app/src/test/java/com/udacity/project4/data/repository/RemindersLocalRepositoryTest.kt
+++ app/src/test/java/com/udacity/project4/data/repository/RemindersLocalRepositoryTest.kt
@@ -1,10 +1,120 @@
 package com.udacity.project4.data.repository
 
-import com.udacity.project4.data.database.FakeRemindersDataSource
+import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.udacity.project4.MainCoroutineRule
+import com.udacity.project4.data.database.RemindersDao
+import com.udacity.project4.data.database.RemindersDatabase
 import com.udacity.project4.data.model.entity.ReminderEntity
+import com.udacity.project4.utils.Result
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.notNullValue
+import org.junit.After
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import org.robolectric.annotation.Config
 
+@RunWith(AndroidJUnit4::class)
+@ExperimentalCoroutinesApi
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
+@MediumTest
 class RemindersLocalRepositoryTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    // Class Under Test
+    private lateinit var remindersRepository: RemindersLocalRepository
+
+    private lateinit var remindersDatabase: RemindersDatabase
+    private lateinit var remindersDao: RemindersDao
+
+
+    @Before
+    fun createRepository() {
+        stopKoin()
+        remindersDatabase = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(), RemindersDatabase::class.java
+        )
+            .allowMainThreadQueries()
+            .build()
+
+        remindersDao = remindersDatabase.reminderDao()
+        remindersRepository = RemindersLocalRepository(remindersDao, Dispatchers.Main)
+    }
+
+    @After
+    fun closeDatabase() {
+        remindersDatabase.close()
+    }
+
+    @Test
+    fun saveReminders_getReminders_returnList() = mainCoroutineRule.runBlockingTest {
+        // GIVEN -- call saveReminder to add list of reminders to database
+        remindersList.forEach {
+            remindersRepository.saveReminder(it)
+        }
+        // WHEN -- call getReminders to pull list from database
+        val result = remindersRepository.getReminders() as Result.Success<List<ReminderEntity>>
+        val data = result.data
+
+        // THEN -- check that data was retrieved
+        assert(data.isNotEmpty())
+        assert(data.size == remindersList.size)
+    }
+
+    @Test
+    fun saveReminder_getReminder_returnSingleReminder() = mainCoroutineRule.runBlockingTest {
+        // GIVEN -- call saveReminder to save a single Reminder
+        remindersRepository.saveReminder(reminder1)
+
+        // WHEN -- call getReminderById to retrieve that reminder
+        val result = remindersRepository.getReminder(reminder1.uid) as Result.Success<ReminderEntity>
+        val savedReminder = result.data
+
+        // THEN -- savedReminder should be match reminder1
+        assertThat(savedReminder, notNullValue())
+        assertThat(savedReminder.uid, `is`(reminder1.uid))
+        assertThat(savedReminder.title, `is`(reminder1.title))
+        assertThat(savedReminder.description, `is`(reminder1.description))
+        assertThat(savedReminder.location, `is`(reminder1.location))
+        assertThat(savedReminder.latitude, `is`(reminder1.latitude))
+        assertThat(savedReminder.longitude, `is`(reminder1.longitude))
+    }
+
+    @Test
+    fun deleteAllReminders_addRemindersList_returnEmptyList() = mainCoroutineRule.runBlockingTest {
+        // GIVEN -- call saveReminder to add list of reminders to database
+        remindersList.forEach {
+            remindersRepository.saveReminder(it)
+        }
+
+        // WHEN -- deleteAllReminders is called
+        remindersRepository.deleteAllReminders()
+        
+        //THEN -- no reminders should return when getReminders is called
+        val list = remindersRepository.getReminders() as Result.Success<List<ReminderEntity>>
+
+        assert(list.data.isEmpty())
+    }
+
+    /**
+     *  Fake Data
+     */
 
     private val reminder1 = ReminderEntity(
         1000,
@@ -34,17 +144,5 @@ class RemindersLocalRepositoryTest {
     )
 
     private val remindersList = listOf(reminder1, reminder2, reminder3)
-    
-    private lateinit var fakeRemindersDataSource: FakeRemindersDataSource
-
-    // Class Under Test
-    private lateinit var remindersRepository: RemindersLocalRepository
-
-    @Before
-    fun createRepository() {
-        fakeRemindersDataSource = FakeRemindersDataSource(remindersList.toMutableList())
-
-//        remindersRepository = RemindersLocalRepository(fakeRemindersDataSource)
-    }
 
 }
